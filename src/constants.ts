9a93b86647ebad5f143f9b58a2b141d694fb4005
Jala30 <104975681+Jala30@users.noreply.github.com>
2024-09-29 21:24:03 +0530

    changed file paths

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const PLUS_ICON = 'url("./assets/images/icon-plus.svg")';
-export const MINUS_ICON = 'url("./assets/images/icon-minus.svg")';
+export const PLUS_ICON = 'url("/faq-accordian/assets/images/icon-plus.svg")';
+export const MINUS_ICON = 'url("/faq-accordian/assets/images/icon-minus.svg")';
